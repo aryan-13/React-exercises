7f03f7c700b2848882e98a1d0ba02d3925e27664
aryan-13 <65868189+aryan-13@users.noreply.github.com>
2022-02-17 20:21:12 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import "./styles.css";
+import React, { useState } from "react";
+
+export default function App() {
+  const [pass, setPass] = useState("");
+
+  const inputHandler = (e) => {
+    setPass(e.target.value);
+    console.log(pass);
+  };
+  return (
+    <div className="App">
+      <h1 style={{ color: "navy" }}>Hello CodeSandbox</h1>
+      <input onChange={inputHandler} />
+      {pass.match(/^(?=.*[a-zA-Z])(?=.*[0-9])[A-Za-z0-9]+$/) ? (
+        <h4 style={{ color: "green" }}>Alphanumeric</h4>
+      ) : (
+        <h4 style={{ color: "red" }}>NOT Alphanumeric</h4>
+      )}
+    </div>
+  );
+}
